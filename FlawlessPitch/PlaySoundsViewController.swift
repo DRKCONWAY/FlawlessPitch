d4579ad6d8ab71d57fa9a28896163e71619e5a4a
DEREK CONWAY <D@dereks-mbp>
2016-04-19 21:52:20 -0500

    the finished product

--- FlawlessPitch/PlaySoundsViewController.swift
+++ FlawlessPitch/PlaySoundsViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-var recordedAudioURL: NSURL!
+    
+    @IBOutlet weak var darthVader: UIButton!
+    @IBOutlet weak var snail: UIButton!
+    @IBOutlet weak var reverb: UIButton!
+    @IBOutlet weak var echo: UIButton!
+    @IBOutlet weak var chipmunk: UIButton!
+    @IBOutlet weak var rabbit: UIButton!
+    @IBOutlet weak var stopRecording: UIButton!
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("play sound button pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        
+        }
+        configureUI(.Playing)
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("stop audio button pressed")
+        stopAudio()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("PlaySoundsViewController loaded")
+        setupAudio()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
     }
 
     override func didReceiveMemoryWarning() {
